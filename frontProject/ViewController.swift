fffcdd71fac630ad5d46a2ad6bb38c3491952270
Helliud <javiereliudliz@gmail.com>
2022-09-08 09:56:44 -0700

    images

--- frontProject/ViewController.swift
+++ frontProject/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgGuacamole: UIImageView!
+    var secGuacamole : [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,15 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func doTapGuacamole(_ sender: Any) {
+        for i in 1...11 {
+            let imagen = UIImage (named: "guacamole\(i)")
+            secGuacamole.append(imagen!)
+        }
+        
+        imgGuacamole.animationImages = secGuacamole
+        imgGuacamole.animationDuration = 1.0
+        imgGuacamole.startAnimating()
+    }
 }
 
